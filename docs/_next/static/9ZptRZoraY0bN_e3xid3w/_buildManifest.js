35cbdda8aa56e6566da77b95443f0e5aab4818c2
Jack Leslie <jack.leslie@transferwise.com>
2022-03-14 22:58:47 +0000

    remove static

--- docs/_next/static/9ZptRZoraY0bN_e3xid3w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/988-d801929c7e64354c.js","static/chunks/pages/index-85c183bf7fb70dbb.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
