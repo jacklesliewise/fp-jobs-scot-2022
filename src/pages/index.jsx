27623cb13906481071f1b4a687a7572becf60861
Jack Leslie <jack.leslie@transferwise.com>
2022-03-15 01:02:51 +0000

    linting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,36 @@
+/* eslint-disable react/forbid-dom-props */
 import Head from 'next/head';
 import { useState } from 'react';
+
 import { jobMap } from '../data/jobMap';
 
 function debounce(func, wait) {
   let timeout;
 
-  return function() {
+  return function () {
     const context = this;
     const args = arguments;
 
     clearTimeout(timeout);
 
-    timeout = setTimeout(function() {
+    // eslint-disable-next-line fp/no-mutation
+    timeout = setTimeout(function () {
       func.apply(context, args);
     }, wait);
-  }
+  };
 }
 
-export default function Home() {
+const Home = () => {
   const [state, setState] = useState('');
 
   const debouncedSetState = debounce((value) => setState(value), 500);
 
   const filterByState = (content, location) => {
-    return content.toLowerCase().includes(state.toLowerCase()) || location.toLowerCase().includes(state.toLowerCase())
-  }
+    return (
+      content.toLowerCase().includes(state.toLowerCase()) ||
+      location.toLowerCase().includes(state.toLowerCase())
+    );
+  };
 
   return (
     <>
@@ -32,54 +38,60 @@ export default function Home() {
         <title>Scotland Foundation Programmes 2022</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       <header style={{ padding: '10px 20px' }}>
-        <h1>Scotland Foundation Programmes 2022</h1>  
-        <input aria-label='Filter by text' type="search" onChange={e => {
-          const { value } = e.target;
+        <h1>Scotland Foundation Programmes 2022</h1>
+        <input
+          aria-label="Filter by text"
+          type="search"
+          onChange={(e) => {
+            const { value } = e.target;
 
-          if (value.length > 0) {
-            debouncedSetState(value)
-          } else {
-            setState(value);
-          }
-        }} placeholder="Search by job content or location..." />
+            if (value.length > 0) {
+              debouncedSetState(value);
+            } else {
+              setState(value);
+            }
+          }}
+          placeholder="Search by job content or location..."
+        />
       </header>
       <main style={{ padding: '0px 20px', fontFamily: '' }}>
         {Object.entries(jobMap).map(([area, group]) => {
-          const groups = Object.entries(group).filter(([_, { content, location }]) => filterByState(content, location)).map(([group, { location, content }]) => (
-            <li style={{ marginTop: '50px' }} key={group}>
-              <h2>{group}</h2>
-              <h3>{location}</h3>
-              <p>{content.split('F2')[0]}</p>
-              <p>F2{content.split('F2')[1]}</p>
-            </li>
-          ));
-          
+          const groups = Object.entries(group)
+            .filter(([_, { content, location }]) => filterByState(content, location))
+            .map(([group, { location, content }]) => (
+              <li style={{ marginTop: '50px' }} key={group}>
+                <h2>{group}</h2>
+                <h3>{location}</h3>
+                <p>{content.split('F2')[0]}</p>
+                <p>F2{content.split('F2')[1]}</p>
+              </li>
+            ));
+
           if (groups.length > 0) {
             return (
               <details key={area} open>
                 <summary>{area}</summary>
-                <ul>
-                  {groups}
-                </ul>
+                <ul>{groups}</ul>
               </details>
-            )
+            );
           }
 
           return null;
         })}
       </main>
-      <style jsx global>{`
+      <style jsx global>
+        {`
           html,
           body {
             padding: 0;
             margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-              Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+              Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
             color: #222222;
           }
 
@@ -123,9 +135,10 @@ export default function Home() {
             margin-bottom: 20px;
             box-shadow: none;
           }
-          
         `}
       </style>
     </>
-  )
-}
+  );
+};
+
+export default Home;
